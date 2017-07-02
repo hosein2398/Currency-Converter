005982b687db37f4dbd2eeff0d57403e932cc336
Hosein <hosein2398@gmail.com>
2017-07-02 23:52:54 +0330

    ava replaced

--- test/test.js
+++ test/test.js
@@ -1,12 +1,11 @@
-var test = require('tape');
-var lib = require('../index.js');
-test('timing test', function (t) {
-    t.plan(2);
-    // t.equal(typeof lib() , null);
-    t.equal(typeof Date.now, 'function');
-    var start = Date.now();
-    
-    setTimeout(function () {
-        t.equal(Date.now() - start, 100);
-    }, 100);
+import test from 'ava';
+
+test('foo', t => {
+	t.pass();
+});
+
+test('bar', async t => {
+	const bar = Promise.resolve('bar');
+
+	t.is(await bar, 'bar');
 });
