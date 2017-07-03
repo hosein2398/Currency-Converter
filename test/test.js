81de3746c6296c0102accb52846743253a771b99
Hosein <hosein2398@gmail.com>
2017-07-03 13:44:59 +0330

    test is done

--- test/test.js
+++ test/test.js
@@ -1,11 +1,25 @@
 import test from 'ava';
+var request = require('request');
+var cheerio = require('cheerio');
 
-test('foo', t => {
-	t.pass();
-});
+test('returned value should be a number', t => {
+	return new Promise((resolve, reject) => {
+		request({
+			method: 'GET',
+			url: 'http://www.xe.com/currencyconverter/convert/?Amount=100&From=usd&To=cop',
+		}, function (err, response, body) {
+			var test_if_title = false;
+			if (err) return console.error(err);
+			var $ = cheerio.load(body);
+			var title = $('.wrapper .bodyContent .quickFixesTopModule .uccResultContainer #ucc-container .uccResultAmount').eq(0).text();
+			setTimeout(
+				function () {
 
-test('bar', async t => {
-	const bar = Promise.resolve('bar');
+					resolve(title);
+				}, 15000);
 
-	t.is(await bar, 'bar');
+		});
+	}).then(function (a) {
+		t.is(typeof +a, 'number');
+	});
 });
